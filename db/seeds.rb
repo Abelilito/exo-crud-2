160f2d77370e438be3e319949a47a6816847fc44
Abel Ilito <ilitoabel@gmail.com>
2021-02-26 17:14:17 +0100

    Create Model; migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+10.times do
+  post = Post.new(
+    title: Faker::Name.name,
+    content: Faker::Lorem.unique.exclude,
+  )
+  articles.save
+end
